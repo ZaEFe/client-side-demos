2a6da8abf7b832b4e43576273749ee2e14c59768
Zachary <zef20@email.vccs.edu>
2025-02-04 23:33:56 -0500

    Actually fixed errors. Didn't run build

---
+++ docs/assets/main-CC17C8XN.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./lodash-BVhXyF_N.js";alert(e.join(["Hello","Checkout the project pages."],"! "));
---
+++ docs/assets/project1-DBvp4Jmx.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./lodash-BVhXyF_N.js";alert(e.join(["This is an alert","Project 2 will be different","Check it out!"],". "));
