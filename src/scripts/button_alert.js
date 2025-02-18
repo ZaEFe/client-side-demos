caeb002b1147dd7e53cff64762e92a01d001a46f
Zachary <zef20@email.vccs.edu>
2025-02-18 18:51:05 -0500

    Created script file instead of using Lodash on Project 2

---
+++ src/scripts/button_alert.js
@@ -0,0 +1,10 @@
+function showAlert() {
+    // Convert the timestamp to a readable date
+    const date = new Date().toLocaleString();
+    // Concat message with current date
+    const message = `Hello, today's date is ${date}`;
+    alert(message);
+}
+
+const dateButton = document.getElementById("dateButton");
+dateButton.addEventListener("click", showAlert);
