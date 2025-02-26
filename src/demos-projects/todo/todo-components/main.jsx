032709812293f9b2329560de1e446c42afdd5ecf
Zachary <zef20@email.vccs.edu>
2025-02-26 17:23:54 -0500

    Figured out the needed file structure to get the app working from navbar

--- src/demos-projects/todo/todo-components/main.jsx
+++ src/demos-projects/todo/todo-components/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { useState, useEffect, useRef } from 'react';
-import './index.css';
+import '../../../styles/todo.css';
 import { openDB, deleteDB } from 'idb';
 
 
